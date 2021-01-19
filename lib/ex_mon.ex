7e4fa7861accc58de642e711131aab08199d04df
Leonardo <leonardo.gottardi@vilaapps.com.br>
2021-01-19 08:20:57 -0300

    adicionei metodo attack e heal

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -2,8 +2,8 @@ defmodule ExMon do
   alias ExMon.{Player, Game}
   alias ExMon.Game.{Actions, Status}
 
-
   @computer_name "Robotink"
+  @computer_moves [:move_avg, :move_rnd, :move_heal]
 
   def create_player(name, move_avg, move_rnd, move_heal) do
     Player.build(name, move_avg, move_rnd, move_heal)
@@ -14,20 +14,32 @@ defmodule ExMon do
     |> create_player(:punch, :kick, :heal)
     |> Game.start(player)
 
-    Status.print_round_message()
+    Status.print_round_message(Game.info())
   end
 
   def make_move(move) do
     move
     |> Actions.fetch_move()
     |> do_move()
+
+    computer_move(Game.info())
   end
 
   def do_move({:error, move}), do: Status.print_wrong_move_message(move)
+
   def do_move({:ok, move}) do
     case move do
-      :move_heal -> "realiza_cura"
+      :move_heal -> Actions.heal()
       move -> Actions.attack(move)
     end
+
+    Status.print_round_message(Game.info())
   end
+
+  defp computer_move(%{turn: :computer, status: :continue}) do
+    move = {:ok, Enum.random(@computer_moves)}
+    do_move(move)
+  end
+
+  defp computer_move(_), do: :ok
 end
--- lib/game/actions.ex
+++ lib/game/actions.ex
@@ -1,11 +1,22 @@
 defmodule ExMon.Game.Actions do
   alias ExMon.Game
+  alias ExMon.Game.Actions.{Attack, Heal}
 
   def attack(move) do
-    
+    case Game.turn() do
+      :player -> Attack.attack_opponent(:computer, move)
+      :computer -> Attack.attack_opponent(:player, move)
+    end
   end
 
+  def heal() do
+    case Game.turn() do
+      :player -> Heal.heal_life(:player)
+      :computer -> Heal.heal_life(:computer)
+    end
+  end
 
+  @spec fetch_move(any) :: any
   def fetch_move(move) do
     Game.player()
     |> Map.get(:moves)
